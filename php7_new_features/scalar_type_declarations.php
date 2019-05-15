c10bd9e6c8d90127b51db3eec72ffbf1043f7aa9
Amit Joshi <34548066+amitjoc@users.noreply.github.com>
2019-05-15 14:39:32 +0530

    test the scalar type declaration.... to check the strict mode you have to use declare statement or just uncomment in script

--- php7_new_features/scalar_type_declarations.php
+++ php7_new_features/scalar_type_declarations.php
@@ -1,4 +1,29 @@
 <?php 
+ini_set('error_reporting', E_ALL & ~E_NOTICE);
+ 
+/**
+ *  @author Amit Joshi
+ *  
+ *  Two types of the scalar type declatation 
+ *      1. coercive (default) 
+ *      2. strict  declare(strict_types=1);
+ *      
+ */
 
-echo "Checking out the new feature of php 7";
+//declare(strict_types=1);  
+// this declaration must be the first statement in script
+// to check the default coercive just comment the above line
+
+function getSumofInt( int $a , int $b)
+{
+    return ($a + $b);
+}
+
+//the default coercive
+echo 'Default Coercie : ' .PHP_EOL ;
+echo getSumofInt(4, 5).PHP_EOL;
+echo getSumofInt(4, '5 person').PHP_EOL;
+echo getSumofInt(4, 5.5).PHP_EOL;
+echo getSumofInt(4.5, 5.2).PHP_EOL;
+ 
 ?>
